df3e7c611941f7f981b354fabb4396fea0224d1c
David Aydin <david.aydin6@gmail.com>
2022-06-13 11:02:07 -0400

    Setup model and routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Dependencies
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require("cors");
+const morgan = require("morgan");
 
 
 // Initialize the Express App
@@ -20,12 +22,67 @@ mongoose.connection
     .on("connected", () => console.log("Connected to MongoDB"))
     .on("close", () => console.log("You are disconnected from MongoDB"))
     .on("error", (error) => console.log("Error with MongoDB: " + error));
+
+
+
+// Set up our model
+const peopleSchema = new mongoose.Schema({
+    name: String,
+    imgage: String,
+    title: String
+}, {timestamps: true});
+
+const People = mongoose.model("People", peopleSchema)
+
+
 // Mount Middleware
+    /*  app.use(express.urlencoded({extended: false}))
+        only when express is serving HTML   */
+
+app.use(cors());// Access-Control-Allow
+app.use(morgan("dev"));
+app.use(express.json()); // Creates req.body from incomoing JSON request bodies
+
 
 // Mount Routes
 app.get("/", (req,res) => {
     res.send("Hello World");
 });
+
+// Index
+app.get("/people", async (req,res) => {
+
+    try {
+        const people = await People.find({});
+        res.send(people);
+    } catch (error) {
+        console.log("error:", error);
+        res.send({error: "Something Went Wrong - Check Console"});
+    }
+});
+
+// Non async await version
+// app.get("/people", (req,res) => {
+//     People.find({}, (err, people) => {
+//         res.send(people);
+//     });
+// });
+
+
+
+// Create
+app.post("/people", async (req,res) => {
+    try {
+        const person = await People.create(req.body);
+        res.send(person);
+    } catch (error) {
+        console.log('error: ', error);
+        res.send({error: "Something Went Wrong - Check Console"});
+    }
+})
+// Update
+// Delete
+
 
 // Tell Express to Listen
 app.listen(PORT, () => {
